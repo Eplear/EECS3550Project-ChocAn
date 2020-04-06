c76ea12bc78b6029519dfd7bf0c9a1a08b11be4d
adamboyd624 <42520070+adamboyd624@users.noreply.github.com>
2020-04-06 15:27:57 -0400

    some initial classes

---
+++ ChocAn1/Provider.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChocAn
+{
+    class Provider
+    {
+        protected string Name { get; set; }
+        protected int Number { get; set; }
+        protected string Address { get; set; }
+        protected string City { get; set; }
+        protected string State { get; set; }
+        protected int Zip { get; set; }
+
+        public Provider(string name, int number, string address, string city, string state, int zip)
+        {
+            this.Name = name;
+            this.Number = number;
+            this.Address = address;
+            this.City = city;
+            this.State = state;
+            this.Zip = zip;
+        }
+    }
+}
+}
