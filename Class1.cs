de83a267dbb30a168cc6848cb5ccc05413d78e76
Trey Troyer <ttroyer2@rockets.utoledo.edu>
2020-04-06 13:58:59 -0400

    Trey testing for sanity

---
+++ Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EECS3550Project_ChocAn
+{
+    class TreyTester
+    {
+        //This is only a test.
+        int testInt = 3;
+        
+    }
+}
