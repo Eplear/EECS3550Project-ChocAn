634d561dc76d9378750e5e54e735fa00f63c32e9
adamboyd624 <42520070+adamboyd624@users.noreply.github.com>
2020-04-07 15:23:37 -0400

    Some Initial Classes

---
+++ ChocAn/Member.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChocAn
+{
+    class Member
+    {
+        protected string Name { get; set; }
+        protected int Number { get; set; }
+        protected string Address { get; set; }
+        protected string City { get; set; }
+        protected string State { get; set; }
+        protected int Zip { get; set; }
+
+        public Member(string name, int number, string address, string city, string state, int zip)
+        {
+            this.Name = name;
+            this.Number = number;
+            this.Address = address;
+            this.City = city;
+            this.State = state;
+            this.Zip = zip;
+        }
+    }
+}
---
+++ ChocAn/Service.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChocAn
+{
+    class Service
+    {
+        protected DateTime DateOfService { get; set; }
+        protected DateTime DateReceived { get; set; }
+        protected string Computer { get; set; }
+        protected string ProviderName { get; set; }
+        protected string ServiceName { get; set; }
+        protected string MemberName { get; set; }
+        protected int MemberNumber { get; set; }
+        protected int ServiceCode { get; set; }
+        protected double Fee { get; set; }
+
+        public Service(DateTime dateOfService, DateTime dateReceived, string computer, string providerName,
+            string serviceName, string memberName, int memberNumber, int serviceCode, double fee)
+        {
+            DateOfService = dateOfService;
+            DateReceived = dateReceived;
+            Computer = computer;
+            ProviderName = providerName;
+            ServiceName = serviceName;
+            MemberName = memberName;
+            MemberNumber = memberNumber;
+            ServiceCode = serviceCode;
+            Fee = fee;
+        }
+    }
+}
