2bbfe0cffd0ac63593ed8cc38dd7887ddaa73df1
Trey Troyer <ttroyer2@rockets.utoledo.edu>
2020-04-23 17:05:19 -0400

    Added more unit tests

--- ChocAn/UnitTestProject1/UnitTest1.cs
+++ ChocAn/UnitTestProject1/UnitTest1.cs
@@ -8,10 +8,51 @@ namespace UnitTestProject1
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void InitializeDataCenter()
         {
             DataCenter dataCenter = new DataCenter();
-            
+
+        }
+
+        [TestMethod]
+        public void ValidateExistingProvider()
+        {
+            DataCenter dataCenter = new DataCenter();
+            dataCenter.ValidateProvider("123456789");
+
+        }
+
+        [TestMethod]
+        public void ValidateNonExistingProvider()
+        {
+            DataCenter dataCenter = new DataCenter();
+            dataCenter.ValidateProvider("000000001");
+
+        }
+
+        [TestMethod]
+        public void ValidateExistingMember()
+        {
+            DataCenter dataCenter = new DataCenter();
+            dataCenter.ValidateMember("123456789");
+
+        }
+
+        [TestMethod]
+        public void ValidateNonExistingMember()
+        {
+            DataCenter dataCenter = new DataCenter();
+            dataCenter.ValidateMember("000000001");
+
+        }
+
+        [TestMethod]
+        public void AddNewMember()
+        {
+            DataCenter dataCenter = new DataCenter();
+            dataCenter.ValidateMember("000000001");
+
         }
     }
 }
+ 
